f9c2b4d749ffd9c1e354da7d9bc31046a70de9d3
Darin Pope <darin@planetpope.com>
2019-08-09 01:13:49 -0400

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,10 +1,25 @@
 <?php
+function stest($ip, $portt) {
+  $fp = @fsockopen($ip, $portt, $errno, $errstr, 0.1);
+  if (!$fp) {
+      return false;
+  } else {
+      fclose($fp);
+      return true;
+  }
+}
 
 $ip = gethostbyname('internal-dtown-staging-intAlb-ea62a2f-1974618375.us-east-2.elb.amazonaws.com');
 echo $ip;
 
 $result = dns_get_record("internal-dtown-staging-intAlb-ea62a2f-1974618375.us-east-2.elb.amazonaws.com");
 print_r($result);
+
+if(stest("internal-dtown-staging-intAlb-ea62a2f-1974618375.us-east-2.elb.amazonaws.com",5000)) {
+  print "I can see port 5000";
+} else {
+  print "I cannot see port 5000";
+}
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 0); 
