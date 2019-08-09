1f1f0b57f9ab97da626679647c253486461d9a83
Darin Pope <darin@planetpope.com>
2019-08-09 00:07:49 -0400

    Create index.php

---
+++ src/index.php
@@ -0,0 +1,9 @@
+<?php
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_URL,"http://internal-dtown-staging-intAlb-ea62a2f-1974618375.us-east-2.elb.amazonaws.com:5000/route/v1/driving/7.436828612,43.739228054975506;7.417058944702148,43.73284046244549?steps=true");
+$result=curl_exec($ch);
+curl_close($ch);
+
+var_dump(json_decode($result, true));
+?>
