b4ba864699cb2e5a302397b17763c57e157668f9
Darin Pope <darin@planetpope.com>
2019-08-09 00:28:59 -0400

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,5 +1,7 @@
 <?php
 $ch = curl_init();
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 0); 
+curl_setopt($ch, CURLOPT_TIMEOUT, 5);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_URL,"http://internal-dtown-staging-intAlb-ea62a2f-1974618375.us-east-2.elb.amazonaws.com:5000/route/v1/driving/7.436828612,43.739228054975506;7.417058944702148,43.73284046244549?steps=true");
 $result=curl_exec($ch);
